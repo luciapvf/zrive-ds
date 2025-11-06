c85852d46bfea590861b2c17483c7874d97abafe
luciapvf <luciapvf@gmail.com>
2025-11-06 19:23:03 +0100

    Guardar cambios en inventory.parquet antes de cambiar de rama

---
+++ src/module_2/congif_template.py
@@ -0,0 +1,5 @@
+# Plantilla de configuración: sustituir por variables de entorno
+import os
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
