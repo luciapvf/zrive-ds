62157fbf76bd3ae8156d9c7f111dea2eed9f3eb2
luciapvf <luciapvf@gmail.com>
2025-11-22 19:17:20 +0100

    Leave only module_2 in this branch

--- src/module_2/congif_template.py
+++
@@ -1,5 +0,0 @@
-# Plantilla de configuración: sustituir por variables de entorno
-import os
-
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
