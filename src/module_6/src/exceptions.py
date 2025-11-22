4b50e32c2a17aefda54b27f58c4f0a578d060c1d
luciapvf <luciapvf@gmail.com>
2025-11-22 21:17:53 +0100

    Remove module_2 to module_6, keep only module_1

--- src/module_6/src/exceptions.py
+++
@@ -1,9 +0,0 @@
-# src/exceptions.py
-
-class PredictionException(Exception):
-    """Excepción personalizada para errores de predicción en BasketModel."""
-    pass
-
-class UserNotFoundException(Exception):
-    """Excepción personalizada para usuario no encontrado en FeatureStore."""
-    pass
